4801299b6d57d6f7a94bd25b456708874a783992
Max <dhi2@users.noreply.github.com>
2017-09-27 02:57:32 -0400

    Update gravityFormProxies.js

--- js/gravityFormProxies.js
+++ js/gravityFormProxies.js
@@ -1,30 +1,6 @@
 /*
  * Github: https://github.com/zenithtech/wordpress-gravity-forms-buttons
  *
- * This is a jQuery-based script and CSS that enables customizing the interface of WordPress Gravity Form radio options and dropdown select fields
- * by creating proxy interfaces that convey the user choices to the original fields
- * and also trigger any related field rules that handle conditional logic.
- *
- * Usage: window.gravityFormProxies(
- * 		[string/boolean: either the classname of a target 'Drop Down' type Gravity form field, or a boolean to target all the fields 'Drop Down' type Gravity form fields],
- * 		[string/boolean: either the classname of a target 'Radio' type Gravity form field, or a boolean to target all 'Radio' type Gravity form fields]
- * 	);
- *
- * If you want to turn all dropdowns in to horizontal buttons, enter true, such as:
- * Example: window.gravityFormProxies(true);
- *
- * Or target a single dropdown by adding a 'Custom CSS Class' to a 'Drop Down' type Gravity form field to turn it into horizontal buttons:
- * Example: window.gravityFormProxies('.gfield.dropdown_buttons');
- *
- * You may also choose to only turn all radio buttons to custom horizontal buttons by adding a second value as true:
- * Example: window.gravityFormProxies(false, true);
- *
- * Or target a single radio group by adding a 'Custom CSS Class' to a 'Radio' type Gravity form field to turn it into horizontal buttons:
- * Example: window.gravityFormProxies(false, 'myVerticalRadios');
- *
- * Or turn all dropdowns and radio buttons into cutom horizontal buttons by passing both values as true:
- * Example: window.gravityFormProxies(true, true);
- *
  */
 
 window.gravityFormProxies = function(dropdowns, radios) {
